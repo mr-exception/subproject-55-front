1bf09b951f3244aa0a7970c8949d9c4f8f926e91
mr-exception <darbandi1996@gmail.com>
2018-05-06 23:46:47 +0430

    mongodb added

--- config.py
+++ config.py
@@ -3,3 +3,20 @@ consumer_secret = 'ubd5BrBlau0Wky2rGY5rygJCdhFWllPJNFc20ctsbqF35Dyl97'
 
 access_token = '730997261333843968-U4hsgX2UpgMmZUPAHTSHbOugaAEyhR1'
 access_token_secret = 'IGVY2WR0MGAofsKHvvDQUDxAIqlbbqJBqnhUIbLWwdlWy'
+
+
+# mongodb configs
+db_host = 'localhost'
+db_name = 'tina'
+db_pass = None
+db_user = None
+db_port = '27017'
+db_auth = False
+
+# timing
+crawl_period = 0 # seconds
+
+
+# consts
+QUEUE_NOT_CRAWLED = 1
+QUEUE_CRAWLED = 2
--- main.py
+++ main.py
@@ -1,17 +1,29 @@
 import tweepy
 import json
+from pymongo import MongoClient
 from config import *
+from time import time
 
+print("conntecting to tweeter...")
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
-
 api = tweepy.API(auth)
+print("connected!")
+
+print("connecting to mongodb...")
+mongodb_connection_string = 'mongodb://{0}:{1}'.format(db_host, db_port)
+if db_auth:
+    mongodb_connection_string = 'mongodb://{0}:{1}@{2}:{3}'.format(db_user, db_pass, db_host, db_port)
+client = MongoClient(mongodb_connection_string)
+db = client[db_name]
+print("conncted!")
+print("---------------------------")
 
 # public_tweets = api.home_timeline()
 # for tweet_object in public_tweets:
 #     tweet = tweet_object._json
 #     id = tweet['user']['id']
-#     name = tweet['user']['name']
+#     name = tweet['user']['url']
 #     text = tweet['text']
 #     print("{0}({1}): {2}".format(name, id, text))
 
@@ -20,11 +32,21 @@ api = tweepy.API(auth)
 
 # print(api.trends_closest(35.6215718, 51.3946415))
 
-public_tweets = api.user_timeline(13058682)
+# public_tweets = api.user_timeline(13058682)
 # public_tweets = api.home_timeline()
-for tweet_object in public_tweets:
-    tweet = tweet_object._json
-    id = tweet['user']['id']
-    name = tweet['user']['name']
-    text = tweet['text']
-    print("{0}({1}): {2}".format(name, id, text))
+# for tweet_object in public_tweets:
+#     tweet = tweet_object._json
+#     id = tweet['user']['id']
+#     name = tweet['user']['name']
+#     text = tweet['text']
+#     print("{0}({1}): {2}".format(name, id, text))
+
+queue = db.queue.find({"status": QUEUE_NOT_CRAWLED}).limit(10)
+for task in queue:
+    print("task {0}".format(task['_id']))
+    print("crawling person -> {0}".format(task['tw_id']))
+    user = api.get_user(task['tw_id'])
+    user_json = user._json
+    print("crawled: {0}".format(user.name))
+    person_id = db.person.insert(user_json)
+    print("inserted in db -> {0}".format(person_id))
