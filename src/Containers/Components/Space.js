8692b221504584fd74a10d92a8683be91ac077c3
mr-exception <darbandi1996@gmail.com>
2019-07-22 17:59:04 +0430

    #Remove some warnings

--- src/Containers/Components/Space.js
+++ src/Containers/Components/Space.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Row, Col, Card, Image, Table } from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 class Space extends React.Component {
 
   render() {
